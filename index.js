48eeb3754b3e74c53009839d2befe1f30ac68f9c
ashley kildunne <ashleykildunne@ashleys-MBP.attlocal.net>
2021-03-09 07:04:34 -0800

    Psudocode

---
+++ index.js
@@ -0,0 +1,10 @@
+// Insertion Sort
+
+// Sort through an Array. Start at index 1 (second element) so that it can be 
+// compared to index 0 since index 0 is now considered sorted.
+
+// The second index will be run through from RIGHT to LEFT until it finds 
+// the correct location.
+
+// Each element should move over by 1 to make room for the new element being sorted.
+
